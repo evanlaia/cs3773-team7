8a0a7b694e86d94ee1c9f1ebe4a8448ea3cdeec3
pabloevd950 <pabloevd950@gmail.com>
2016-11-23 11:53:43 -0600

    Set up login and Sign in activity.

--- app/src/main/java/thebobs/messaging/MainActivity.java
+++ app/src/main/java/thebobs/messaging/MainActivity.java
@@ -8,41 +8,155 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.EditText;
+import android.support.design.widget.TextInputLayout;
+import android.widget.Toast;
+import android.view.WindowManager;
+import android.util.Log;
+import android.text.TextUtils;
+import android.support.annotation.NonNull;
+import android.content.Intent;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private static final String TAG = "SignupActivity" ;
+    private Button signUpButton,btnLinkToLogIn;
+    private ProgressBar progressBar;
+    private FirebaseAuth auth;
+    private EditText signupInputEmail, signupInputPassword;
+    private TextInputLayout  signupInputLayoutEmail, signupInputLayoutPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+            setContentView(R.layout.activity_main);
+            mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
 
-//        if(FirebaseAuth.getInstance().getCurrentUser() == null) {
-//            // Start sign in/sign up activity
-//            startActivityForResult(
-//                    AuthUI.getInstance()
-//                            .createSignInIntentBuilder()
-//                            .build(),
-//                    SIGN_IN_REQUEST_CODE
-//            );
-//        } else {
-//            // User is already signed in. Therefore, display
-//            // a welcome Toast
-//            Toast.makeText(this,
-//                    "Welcome " + FirebaseAuth.getInstance()
-//                            .getCurrentUser()
-//                            .getDisplayName(),
-//                    Toast.LENGTH_LONG)
-//                    .show();
-//
-//            // Load chat room contents
-//            displayChatMessages();
-//        }
+            auth = FirebaseAuth.getInstance();
 
+            signupInputLayoutEmail = (TextInputLayout) findViewById(R.id.signup_input_layout_email);
+            signupInputLayoutPassword = (TextInputLayout) findViewById(R.id.signup_input_layout_password);
+            progressBar = (ProgressBar) findViewById(R.id.progressBar);
+            signupInputEmail = (EditText) findViewById(R.id.signup_input_email);
+            signupInputPassword = (EditText) findViewById(R.id.signup_input_password);
+
+            signUpButton = (Button) findViewById(R.id.btn_signup);
+            btnLinkToLogIn = (Button) findViewById(R.id.btn_link_login);
+
+
+            signUpButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    submitForm();
+
+                }
+            });
+
+            btnLinkToLogIn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            });
 
     }
+    //
+    private void submitForm() {
+
+        String email = signupInputEmail.getText().toString().trim();
+        String password = signupInputPassword.getText().toString().trim();
+
+        if(!checkEmail()) {
+            return;
+        }
+        if(!checkPassword()) {
+            return;
+        }
+        signupInputLayoutEmail.setErrorEnabled(false);
+        signupInputLayoutPassword.setErrorEnabled(false);
+
+        progressBar.setVisibility(View.VISIBLE);
+        //create user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG,"createUserWithEmail:onComplete:" + task.isSuccessful());
+                        progressBar.setVisibility(View.GONE);
+                        // If sign in fails, Log the message to the LogCat. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.d(TAG,"Authentication failed." + task.getException());
+
+                        } else {
+                            startActivity(new Intent(MainActivity.this, UserActivity.class));
+                            finish();
+                        }
+                    }
+                });
+        Toast.makeText(getApplicationContext(), "You are Registered !!", Toast.LENGTH_SHORT).show();
+    }
+
+    private boolean checkEmail() {
+        String email = signupInputEmail.getText().toString().trim();
+        if (email.isEmpty() || !isEmailValid(email)) {
+
+            signupInputLayoutEmail.setErrorEnabled(true);
+            signupInputLayoutEmail.setError(getString(R.string.err_msg_email));
+            signupInputEmail.setError(getString(R.string.err_msg_required));
+            requestFocus(signupInputEmail);
+            return false;
+        }
+        signupInputLayoutEmail.setErrorEnabled(false);
+        return true;
+    }
+
+    private boolean checkPassword() {
+
+        String password = signupInputPassword.getText().toString().trim();
+        if (password.isEmpty() || !isPasswordValid(password)) {
+
+            signupInputLayoutPassword.setError(getString(R.string.err_msg_password));
+            signupInputPassword.setError(getString(R.string.err_msg_required));
+            requestFocus(signupInputPassword);
+            return false;
+        }
+        signupInputLayoutPassword.setErrorEnabled(false);
+        return true;
+    }
+    //
+
+    private static boolean isEmailValid(String email) {
+        return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+    private static boolean isPasswordValid(String password){
+        return (password.length() >= 6);
+    }
+
+    private void requestFocus(View view) {
+        if (view.requestFocus()) {
+            getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -67,6 +181,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void displayChatMessages() {
+
+    }
+
+    public void createUserWithEmailandPassword(){
 
     }
 
