fb8d6c319105025e6a2176bad9da6ca1c0cccb73
pabloevd950 <pabloevd950@gmail.com>
2016-12-04 20:53:18 -0600

    Notifications and messages from console working

--- app/src/main/java/thebobs/messaging/user2.java
+++ app/src/main/java/thebobs/messaging/user2.java
@@ -2,12 +2,17 @@ package thebobs.messaging;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
 
 public class user2 extends AppCompatActivity {
+    private TextView textView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user2);
+        textView = (TextView)findViewById(R.id.textView1);
+        textView.setText("This is the user activity");
     }
 }
