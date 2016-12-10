4f3157b9828709c7c14c00a4ba3f81918b81096a
nghianguyen <nghianguyen4708@live.com>
2016-12-10 12:13:11 -0600

    +Grabbed Children from server and print contents

--- app/src/main/java/thebobs/messaging/ChatActivity.java
+++ app/src/main/java/thebobs/messaging/ChatActivity.java
@@ -31,31 +31,32 @@ public class ChatActivity extends AppCompatActivity {
 
 
 
+
+
+
+
        // myRef.setValue(value);
         myRef.addValueEventListener(new ValueEventListener() {
+
+
+
+
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-               // String value = dataSnapshot.getValue(String.class);
-                //textView.setText(value+"\n"+"cat!!!!!!");
-
-                //Map<String, Object> td = (HashMap<String,Object>) dataSnapshot.getValue();
-                //List<Object> values = new List<>(td.values());
 
 
-                List<String> lst = new ArrayList<String>(); // Result will be holded Here
+                Map<String, Object> newPost = (Map<String, Object>) dataSnapshot.getValue();
+                List<String> lst = new ArrayList<String>();
+                List<String>  textlist = new ArrayList<String>();// Result will be holded Here
                 for(DataSnapshot dsp : dataSnapshot.getChildren()){
                     lst.add(String.valueOf(dsp.getKey())); //add result into array list
+
                 }
-                Log.d("list", lst.toString());
-
-                //NOW YOU HAVE ARRAYLIST WHICH HOLD RESULTS
-
-
-
+                Log.d("lisrrtertetertt",lst.toString());
+                String childName="";
 
                 for(String data:lst){
+
                     textView.setText(data);
                 }
 
@@ -65,12 +66,60 @@ public class ChatActivity extends AppCompatActivity {
 
             }
 
+
+            private void collectPhoneNumbers(Map<String,Object> users) {
+
+                ArrayList<String> phoneNumbers = new ArrayList<String>();
+
+                //iterate through each user, ignoring their UID
+                for (Map.Entry<String, Object> entry : users.entrySet()){
+
+                    //Get user map
+                    Map singleUser = (Map) entry.getValue();
+                    //Get phone field and append to list
+                    phoneNumbers.add((String)singleUser.get("text"));
+                    System.out.println("cat!!!!!!!!!!!!!!!!!"+phoneNumbers);
+                    Log.d("lissta", phoneNumbers.toString());
+                }
+
+                System.out.println("cat"+phoneNumbers.toString());
+            }
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
             }
         });
+        myRef.addChildEventListener(new ChildEventListener() {
+            // Retrieve new posts as they are added to Firebase
+            @Override
+            public void onChildAdded(DataSnapshot snapshot, String previousChildKey) {
+                Map<String, Object> newPost = (Map<String, Object>) snapshot.getValue();
+                System.out.println("Author: " + newPost.get("name"));
+                System.out.println("Message: " + newPost.get("text"));
+            }
 
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+            //... ChildEventListener also defines onChildChanged, onChildRemoved,
+            //    onChildMoved and onCanceled, covered in later sections.
+        });
 
 
 
